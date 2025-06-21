967f2cc1fd46780d8fa1d6ca7ccef7e23d76e9cf
Natalia2301010264 <nataliametan04@gmail.com>
2025-06-21 17:08:36 +0800

    oop-pertemuan14
    -

---
+++ oop-pertemuan10new1-REAL-13--main/oop-pert10-new-main/pertemuan10/src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,20 @@
+package pertemuan10;
+
+import static pertemuan10.dbkoneksi.koneksi;
+import java.sql.Connection;
+import java.sql.SQLException;
+/**
+ *
+ * @author I Putu Eka Saputra
+ * TGL : 24 MEI 2025
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+    
+    }
+    
+}
